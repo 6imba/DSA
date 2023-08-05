7e557f3eef75d2daed38ebd2005219e379e3ddba
6imba <simbaamir55@gmail.com>
2023-08-05 16:20:15 +0545

    1stcmt

---
+++ DSA LAB/dsa1.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *next;
+};
+
+class linked_list
+{
+	private:
+		node *head , *tail;
+	public:
+		linked_list()
+		{
+			head=NULL;
+			tail=NULL;
+		}
+		
+		void add_node(int i)
+		{
+			node *tmp=new node;
+			tmp->data=i;
+			tmp->next=NULL;
+			if(head==NULL)
+			{
+				head=tmp;
+				tail=tmp;
+			}
+			else
+			{
+				tail->next=tmp;
+				tail=tail->next;
+			}
+		}
+		
+		void display()
+		{
+			node *tmp;
+			tmp=head;
+			while(tmp!=NULL)
+			{
+				cout<<tmp->data<<"->";
+				tmp=tmp->next;
+			}
+		}
+};
+
+int main()
+{
+	int i;
+	linked_list a;
+	char ch='y';
+	while(ch=='y'|| ch=='Y')
+	{
+		cout<<"Enter the number to insert in linked list"<<endl;
+		cin>>i;
+		a.add_node(i);
+		cout<<"Want to insert more node(y/Y)?"<<endl;
+		cin>>ch;
+	}
+	a.display();
+	return 0;
+}
